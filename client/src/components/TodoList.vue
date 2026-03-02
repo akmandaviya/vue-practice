051eda86b43a7c0855676e259e260ab6fb79746d
unknown <abdulmandaviya@gmail.com>
2026-03-02 16:17:49 +0530

    initial

---
+++ client/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Todo List</h1>
+        <input type="text" placeholder="Add something" v-model="newTodo"/>
+        <button @click="addTodo">Add</button>
+
+        <ul v-for="item in todoList" :key="item.id">
+            <li>{{ item.text }}
+                <button @click="removeTodo(item.id)">Remove</button>
+            </li>
+        </ul>
+
+
+        <h1> Data in Table from API</h1>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const todoList = ref([])
+
+const addTodo = () => {
+    if (!newTodo.value) return
+    todoList.value.push({
+        text: newTodo.value,
+        id: Math.random()
+    })
+    newTodo.value = ''
+}
+
+const removeTodo = (id) => {
+    todoList.value = todoList.value.filter(item => item.id !== id)
+}
+
+</script>
