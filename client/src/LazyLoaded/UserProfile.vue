2276f69b0819a96f939a0aab4243f683787299a5
unknown <abdulmandaviya@gmail.com>
2026-03-11 18:10:58 +0530

    suspense for lazy loading

---
+++ client/src/LazyLoaded/UserProfile.vue
@@ -0,0 +1,11 @@
+<script setup>
+const data = await fetch('https://jsonplaceholder.typicode.com/users/1')
+const user = await data.json()
+</script>
+
+<template>
+  <div>
+    <h2>{{ user.name }}</h2>
+    <p>{{ user.email }}</p>
+  </div>
+</template> 
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -12,7 +12,33 @@
             </button>
         </div>
 
-        <Popup v-if="showPopup" @close="showPopup = false" />
+        
+        <div>
+            <button @click="showUserData = true">
+                User Data
+            </button>
+        </div>
+
+        <!-- <Popup v-if="showPopup" @close="showPopup = false" /> -->
+        <Suspense v-if="showPopup">
+            <template #default>
+                <Popup />
+            </template>
+
+            <template #fallback>
+                <div>Loading Popup...</div>
+            </template>
+        </Suspense>
+
+        <Suspense v-if="showUserData">
+            <template #default>
+                <UserProfile />
+            </template>
+
+            <template #fallback>
+                <div>Loading user...</div>
+            </template>
+        </Suspense>
 
     </div>
 </template>
@@ -20,8 +46,11 @@
 import { ref, defineAsyncComponent } from 'vue'
 
 const showPopup = ref(false)
+const showUserData = ref(false)
 
 const Popup = defineAsyncComponent(() =>
     import('../LazyLoaded/Modals/Popup.vue')
 )
+
+const UserProfile = defineAsyncComponent(() => import('../LazyLoaded/UserProfile.vue'))
 </script>
